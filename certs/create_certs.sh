c5ace0b23c7d85713bd1f1693f7fdf03588656a7
Manny Rivera <mannyrivera2010@gmail.com>
2018-01-22 15:50:45 -0500

    feat(certs): add nopass server cert

--- certs/create_certs.sh
+++ certs/create_certs.sh
@@ -3,48 +3,54 @@
 # based on http://nategood.com/client-side-certificate-authentication-in-ngi
 rm *.key *.txt *.pem *.crt *.csr -f
 
-CA_KEY='CA_Root.key'
-CA_SIGNED_CERT='CA_Root'
-CA_SIGNED_CERT_POST='.crt'
-CA_SIGNED_CERT_FILENAME=$CA_SIGNED_CERT$CA_SIGNED_CERT_POST
+CA_SIGNED_CERT='ca_root'
+CA_KEY_FILENAME=$CA_SIGNED_CERT.key
+CA_SIGNED_CERT_FILENAME=$CA_SIGNED_CERT.crt
 CA_SUBJ='/C=US/ST=MD/L=Baltimore/O=AML Demo CA/OU=Domain Control/CN=Demo ALM Root CA/emailAddress=admin@aml.com'
-CA_PASSWORD_FILE='CA_passphrase.txt'
+CA_PASSWORD_FILE='ca_passphrase.txt'
 
 SERVER_KEY='server.key'
 SERVER_REQUEST='server.csr'
 SERVER_SIGNED_CERT='server.crt'
+
+SERVER_NOPASS_KEY='server_nopass.key'
+SERVER_NOPASS_REQUEST='server_nopass.csr'
+SERVER_SIGNED_NOPASS_CERT='server_nopass.crt'
+
 SERVER_SUBJ='/C=US/ST=MD/L=Baltimore/O=AML Server/OU=Domain Control/CN=localhost/emailAddress=admin@aml.com'
 SERVER_PASSWORD_FILE='SERVER_passphrase.txt'
 
 CREATE_CA_ROOT_CERT(){
   # Create the CA Key and Certificate for signing Client Certs
   openssl rand -base64 48 > $CA_PASSWORD_FILE
-  openssl genrsa -des3 -passout file:$CA_PASSWORD_FILE -out $CA_KEY 4096
-  openssl req -new -passin file:$CA_PASSWORD_FILE -x509 -days 365 -key $CA_KEY  -subj "$CA_SUBJ" -out $CA_SIGNED_CERT_FILENAME
+  openssl genrsa -des3 -passout file:$CA_PASSWORD_FILE -out $CA_KEY_FILENAME 4096
+  openssl req -new -passin file:$CA_PASSWORD_FILE -x509 -days 365 -key $CA_KEY_FILENAME  -subj "$CA_SUBJ" -out $CA_SIGNED_CERT_FILENAME
   openssl x509 -in $CA_SIGNED_CERT_FILENAME -out $CA_SIGNED_CERT.pem -outform PEM
 }
 
 CREATE_SERVER_CERT(){
   # Create the Server Key, CSR, and Certificate
+  # We're self signing our own server cert here.  This is a no-no in production.
+  # TODO: Serial 00 B8 FB C1 B7 85 33 54 28
   echo 'password1!' > $SERVER_PASSWORD_FILE
   openssl genrsa -des3 -passout file:$SERVER_PASSWORD_FILE -out $SERVER_KEY 2048
   openssl req -new -passin file:$SERVER_PASSWORD_FILE -key $SERVER_KEY -subj "$SERVER_SUBJ" -out $SERVER_REQUEST
+  openssl x509 -req -days 365 -in $SERVER_REQUEST -passin file:$CA_PASSWORD_FILE -CA $CA_SIGNED_CERT_FILENAME -CAkey $CA_KEY_FILENAME -set_serial 01 -out $SERVER_SIGNED_CERT
 
+  # Create the Server Key, CSR, and Certificate used for apache or ngnix servers
   # We're self signing our own server cert here.  This is a no-no in production.
-  # TODO: Serial 00 B8 FB C1 B7 85 33 54 28
-  openssl x509 -req -days 365 -in $SERVER_REQUEST -passin file:$CA_PASSWORD_FILE -CA $CA_SIGNED_CERT_FILENAME -CAkey $CA_KEY -set_serial 01 -out $SERVER_SIGNED_CERT
-  rm $SERVER_REQUEST
+  openssl genrsa -out $SERVER_NOPASS_KEY 2048
+  openssl req -new -nodes -key $SERVER_NOPASS_KEY -subj "$SERVER_SUBJ" -out $SERVER_NOPASS_REQUEST
+  openssl x509 -req -days 365 -in $SERVER_NOPASS_REQUEST -passin file:$CA_PASSWORD_FILE -CA $CA_SIGNED_CERT_FILENAME -CAkey $CA_KEY_FILENAME -set_serial 01 -out $SERVER_SIGNED_NOPASS_CERT
 }
 
 CREATE_CLIENT_CERT(){
   # Create the Client Key and CSR
-  openssl genrsa -des3 -passout file:$SERVER_PASSWORD_FILE -out $CURRENT_USER_FILENAME.key 2048
-  openssl req -new -passin file:$SERVER_PASSWORD_FILE -key $CURRENT_USER_FILENAME.key -subj "$CURRENT_USER_SUBJ" -out $CURRENT_USER_FILENAME.csr
-
   # We're self signing our own server cert here.  This is a no-no in production.
   # TODO: Serial 00 B8 FB C1 B7 85 33 54 28
-  openssl x509 -req -days 365 -in $CURRENT_USER_FILENAME.csr -passin file:$CA_PASSWORD_FILE -CA $CA_SIGNED_CERT_FILENAME -CAkey $CA_KEY -set_serial 01 -out $CURRENT_USER_FILENAME.crt
-  rm $CURRENT_USER_FILENAME.csr
+  openssl genrsa -des3 -passout file:$SERVER_PASSWORD_FILE -out $CURRENT_USER_FILENAME.key 2048
+  openssl req -new -passin file:$SERVER_PASSWORD_FILE -key $CURRENT_USER_FILENAME.key -subj "$CURRENT_USER_SUBJ" -out $CURRENT_USER_FILENAME.csr
+  openssl x509 -req -days 365 -in $CURRENT_USER_FILENAME.csr -passin file:$CA_PASSWORD_FILE -CA $CA_SIGNED_CERT_FILENAME -CAkey $CA_KEY_FILENAME -set_serial 01 -out $CURRENT_USER_FILENAME.crt
 
   openssl pkcs12 -export -passin file:$SERVER_PASSWORD_FILE -password pass:password -out $CURRENT_USER_FILENAME.p12 -inkey $CURRENT_USER_FILENAME.key -in $CURRENT_USER_FILENAME.crt -certfile $CA_SIGNED_CERT_FILENAME
 }
@@ -55,6 +61,7 @@ CREATE_SERVER_CERT
 ## declare an array variable of users
 declare -a users=(
   "wsmith;Winston Smith wsmith;Minitrue"
+  "bigbrother;Big Brother bigbrother;Minipax"
 )
 
 for user in "${users[@]}"
@@ -68,7 +75,6 @@ do
   echo $CURRENT_USER_FILENAME
   echo $CURRENT_USER_COMMON_NAME
   echo $CURRENT_USER_ORG
-
 
   CREATE_CLIENT_CERT
   # TODO: Finish
