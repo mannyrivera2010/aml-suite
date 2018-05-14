c8a3fb544061bc9e0b93b97a553ff8cf8241acf6
Manny Rivera <mannyrivera2010@gmail.com>
2018-05-14 15:46:58 -0400

    fix(certs): fix certs serial number

--- certs/create_certs.sh
+++ certs/create_certs.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # Script used to create Certificates
 # based on http://nategood.com/client-side-certificate-authentication-in-ngi
+# https://gist.github.com/Soarez/9688998
+
 rm *.key *.txt *.pem *.crt *.csr -f
 
 CA_SIGNED_CERT='ca_root'
@@ -25,7 +27,7 @@ CREATE_CA_ROOT_CERT(){
   openssl rand -base64 48 > $CA_PASSWORD_FILE
   openssl genrsa -des3 -passout file:$CA_PASSWORD_FILE -out $CA_KEY_FILENAME 4096
   openssl req -new -passin file:$CA_PASSWORD_FILE -x509 -days 365 -key $CA_KEY_FILENAME  -subj "$CA_SUBJ" -out $CA_SIGNED_CERT_FILENAME
-  openssl x509 -in $CA_SIGNED_CERT_FILENAME -out $CA_SIGNED_CERT.pem -outform PEM
+  openssl x509 -in $CA_SIGNED_CERT_FILENAME -out $CA_SIGNED_CERT.pem  -outform PEM
 }
 
 CREATE_SERVER_CERT(){
@@ -35,13 +37,13 @@ CREATE_SERVER_CERT(){
   echo 'password1!' > $SERVER_PASSWORD_FILE
   openssl genrsa -des3 -passout file:$SERVER_PASSWORD_FILE -out $SERVER_KEY 2048
   openssl req -new -passin file:$SERVER_PASSWORD_FILE -key $SERVER_KEY -subj "$SERVER_SUBJ" -out $SERVER_REQUEST
-  openssl x509 -req -days 365 -in $SERVER_REQUEST -passin file:$CA_PASSWORD_FILE -CA $CA_SIGNED_CERT_FILENAME -CAkey $CA_KEY_FILENAME -set_serial 01 -out $SERVER_SIGNED_CERT
+  openssl x509 -req -days 365 -in $SERVER_REQUEST -passin file:$CA_PASSWORD_FILE -CA $CA_SIGNED_CERT_FILENAME -CAkey $CA_KEY_FILENAME -CAcreateserial -out $SERVER_SIGNED_CERT
 
   # Create the Server Key, CSR, and Certificate used for apache or ngnix servers
   # We're self signing our own server cert here.  This is a no-no in production.
   openssl genrsa -out $SERVER_NOPASS_KEY 2048
   openssl req -new -nodes -key $SERVER_NOPASS_KEY -subj "$SERVER_SUBJ" -out $SERVER_NOPASS_REQUEST
-  openssl x509 -req -days 365 -in $SERVER_NOPASS_REQUEST -passin file:$CA_PASSWORD_FILE -CA $CA_SIGNED_CERT_FILENAME -CAkey $CA_KEY_FILENAME -set_serial 01 -out $SERVER_SIGNED_NOPASS_CERT
+  openssl x509 -req -days 365 -in $SERVER_NOPASS_REQUEST -passin file:$CA_PASSWORD_FILE -CA $CA_SIGNED_CERT_FILENAME -CAkey $CA_KEY_FILENAME -CAcreateserial -out $SERVER_SIGNED_NOPASS_CERT
 }
 
 CREATE_CLIENT_CERT(){
@@ -49,10 +51,16 @@ CREATE_CLIENT_CERT(){
   # We're self signing our own server cert here.  This is a no-no in production.
   # TODO: Serial 00 B8 FB C1 B7 85 33 54 28
   openssl genrsa -des3 -passout file:$SERVER_PASSWORD_FILE -out $CURRENT_USER_FILENAME.key 2048
+  openssl rsa -in $CURRENT_USER_FILENAME.key -passin file:$SERVER_PASSWORD_FILE -out bigbrother_nopassword.pem
+
   openssl req -new -passin file:$SERVER_PASSWORD_FILE -key $CURRENT_USER_FILENAME.key -subj "$CURRENT_USER_SUBJ" -out $CURRENT_USER_FILENAME.csr
-  openssl x509 -req -days 365 -in $CURRENT_USER_FILENAME.csr -passin file:$CA_PASSWORD_FILE -CA $CA_SIGNED_CERT_FILENAME -CAkey $CA_KEY_FILENAME -set_serial 01 -out $CURRENT_USER_FILENAME.crt
+  openssl x509 -req -days 365 -in $CURRENT_USER_FILENAME.csr -passin file:$CA_PASSWORD_FILE -CA $CA_SIGNED_CERT_FILENAME -CAkey $CA_KEY_FILENAME -CAcreateserial -out $CURRENT_USER_FILENAME.crt
 
   openssl pkcs12 -export -passin file:$SERVER_PASSWORD_FILE -password pass:password -out $CURRENT_USER_FILENAME.p12 -inkey $CURRENT_USER_FILENAME.key -in $CURRENT_USER_FILENAME.crt -certfile $CA_SIGNED_CERT_FILENAME
+
+  openssl x509 -in $CURRENT_USER_FILENAME.crt -noout -text
+  openssl verify -CAfile $CA_SIGNED_CERT_FILENAME $CURRENT_USER_FILENAME.crt
+
 }
 
 CREATE_CA_ROOT_CERT
